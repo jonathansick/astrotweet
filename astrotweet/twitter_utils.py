0a53ffa08f76c023c6bc0d996dc813e40846e8a9
Jonathan Sick <jonathansick@mac.com>
2013-04-27 01:12:59 -0400

    Implement get_friends_ids() and get_follower_ids()
    API for getting list of friends is at
    https://dev.twitter.com/docs/api/1.1/get/friends/ids
    
    API for getting list of followers is at
    https://dev.twitter.com/docs/api/1.1/get/followers/ids
    
    Properly checks the rate limit status for both API resources. Rate limit is
    15 calls every 15 minutes.

--- astrotweet/twitter_utils.py
+++ astrotweet/twitter_utils.py
@@ -83,8 +83,8 @@ def _lookup_user_batch(t, usrBatch):
                 time.sleep(waitPeriod)
                 waitPeriod *= 1.5
                 continue
-            elif t.application.rate_limit_status()['resources']['users']['/users/lookup/']['remaining'] == 0:
-                status = t.application.rate_limit_status()['resources']['users']['/users/lookup/']['remaining']
+            elif t.application.rate_limit_status()['resources']['users']['/users/lookup']['remaining'] == 0:
+                status = t.application.rate_limit_status()['resources']['users']['/users/lookup']
                 now = time.time() # UTC
                 whenRateLimitResets = status['reset'] # UTC
                 sleepTime = whenRateLimitResets - now
@@ -94,14 +94,99 @@ def _lookup_user_batch(t, usrBatch):
     return userData
 
 
+def get_friends_ids(screenName=None, idstr=None, maxFriends=10000):
+    """Given a user screen name or `id_str`, return a list of the user's
+    friends (who the person follows). Each friend is an `id_str`."""
+    t = connect()
+    args = {"stringify_ids": True}
+    if screenName is not None:
+        args['screen_name'] = screenName
+    elif idstr is not None:
+        args['user_id'] = idstr
+    else:
+        # TODO throw exception
+        pass
+    cursor = -1
+    ids = []
+    waitPeriod = 2.
+    while cursor != 0:
+        args['cursor'] = cursor
+        try:
+            result = t.friends.ids(**args)
+            ids.extend(result['ids'])
+            cursor = result['next_cursor']
+            if len(ids) >= maxFriends: break
+        except twitter.api.TwitterHTTPError, e:
+            if e.e.code == 401:
+                print 'Encountered 401 Error (Not Authorized)'
+                break
+            elif e.e.code in (502, 503):
+                print 'Encountered %i Error. Trying again in %i seconds' % (e.e.code, waitPeriod)
+                time.sleep(waitPeriod)
+                waitPeriod *= 1.5
+                continue
+            elif t.application.rate_limit_status()['resources']['friends']['/friends/ids']['remaining'] == 0:
+                status = t.application.rate_limit_status()['resources']['friends']['/friends/ids']
+                now = time.time() # UTC
+                whenRateLimitResets = status['reset'] # UTC
+                sleepTime = whenRateLimitResets - now
+                print 'Rate limit reached. Trying again in %i seconds' % (sleepTime, )
+                time.sleep(sleepTime)
+                continue
+    return ids
+
+
+def get_follower_ids(screenName=None, idstr=None, maxFollowers=10000):
+    """Given a user screen name or `id_str`, return a list of the user's
+    followers. Each follower is an `id_str`.
+    
+    :param maxFollowers": maximum number of followers to be returned.
+    """
+    t = connect()
+    args = {"stringify_ids": True}
+    if screenName is not None:
+        args['screen_name'] = screenName
+    elif idstr is not None:
+        args['user_id'] = idstr
+    else:
+        # TODO throw exception
+        pass
+    cursor = -1
+    ids = []
+    waitPeriod = 2.
+    while cursor != 0:
+        args['cursor'] = cursor
+        try:
+            result = t.followers.ids(**args)
+            ids.extend(result['ids'])
+            cursor = result['next_cursor']
+            if len(ids) >= maxFollowers: break
+        except twitter.api.TwitterHTTPError, e:
+            if e.e.code == 401:
+                print 'Encountered 401 Error (Not Authorized)'
+                break
+            elif e.e.code in (502, 503):
+                print 'Encountered %i Error. Trying again in %i seconds' % (e.e.code, waitPeriod)
+                time.sleep(waitPeriod)
+                waitPeriod *= 1.5
+                continue
+            elif t.application.rate_limit_status()['resources']['followers']['/followers/ids']['remaining'] == 0:
+                status = t.application.rate_limit_status()['resources']['followers']['/followers/ids']
+                now = time.time() # UTC
+                whenRateLimitResets = status['reset'] # UTC
+                sleepTime = whenRateLimitResets - now
+                print 'Rate limit reached. Trying again in %i seconds' % (sleepTime, )
+                time.sleep(sleepTime)
+                continue
+    return ids
+
+
 def str_to_datetime(createdAt):
     """Convert a twitter timestamp (often a ``created_at`` field) to a datetime.
     
     See: http://stackoverflow.com/a/8825799
     """
     return datetime.datetime.strptime(createdAt, '%a %b %d %H:%M:%S +0000 %Y')
-
-
 
 
 def chunks(l, n):
@@ -120,3 +205,5 @@ if __name__ == '__main__':
     status = t.application.rate_limit_status()
     print json.dumps(status, sort_keys=True, indent=1)
     print status['resources']['users']['/users/lookup']
+    print get_friends_ids(screenName='jonathansick')
+    print get_follower_ids(screenName='jonathansick')
